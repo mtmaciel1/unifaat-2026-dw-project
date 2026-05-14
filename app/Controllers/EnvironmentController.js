35f5b002c21293b5277a5a6adb0bc3ab0af01b09
mtmaciel <mtmaciel7@gmail.com>
2026-05-14 13:03:51 -0300

    feat: implementa rota GET /ambiente - TF Aula 10

--- app/Controllers/EnvironmentController.js
+++ app/Controllers/EnvironmentController.js
@@ -4,7 +4,26 @@ import CONSTANTS from '../../bootstrap/config.js';
 
 export default async function EnvironmentController(request, response) {
 
+  const isDocker = process.env.IS_DOCKER === 'true';
 
-    /** TF 10 - Codar aqui */
+  const environment = isDocker ? 'docker' : 'local';
+
+  const dbHost = process.env.POSTGRES_HOST || 'localhost';
+  const dbPort = parseInt(process.env.POSTGRES_PORT) || 6789;
+
+  const webHost = isDocker ? 'nodeweb_host' : 'localhost';
+  const webPort = isDocker ? 8080 : parseInt(process.env.NODE_WEB_PORT) || 3000;
+
+  return response.json({
+    environment,
+    database: {
+      host: dbHost,
+      port: dbPort,
+    },
+    web: {
+      host: webHost,
+      port: webPort,
+    },
+  });
 
 }
